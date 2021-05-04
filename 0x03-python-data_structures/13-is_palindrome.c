6d0bcd9003fbeee62ca922b49cda5d0b6f531bde
root <root@kali-linux>
2021-05-04 23:44:51 +0200

    Data Structures

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,16 +1,30 @@
-
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * is_palindrome - finds palindromism in a list
+ * @head: pointer to the first node
+ * Return: retunr 1 if palindrome, else 0
  */
-int main(void)
-{
-listint_t *head;
 
+int is_palindrome(listint_t **head)
+{
+listint_t *temp = *head;
+int count = 0, len = 0, iter = 0;
+int buffer[9999];
+if (!head || !*head || !temp->next )
+return (1);
+while (temp)
+{
+buffer[count] = temp->n;
+count++;
+temp = temp->next;
+}
+len = count -1;
+while (iter <= len)
+{
+if (buffer[iter] != buffer[len])
 return (0);
+iter++;
+len--;
+}
+return (1);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,7 +1,8 @@
-
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -12,14 +13,14 @@
  */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-
+size_t list_len(const listint_t *h);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
