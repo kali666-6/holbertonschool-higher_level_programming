c66711e7007ab6d22fc23f3e3bf8a741f8cebd01
root <root@kali-linux>
2021-05-21 21:30:38 +0200

    Test-driven development

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+The 5-text_indentation module
+This module has a funciton that indents text
+'''
+
+
+def text_indentation(text):
+    '''
+    The function that that prints My name is <first name> <last name>
+    '''
+
+    st = ''
+    special = {'.', '?', ':'}
+
+    if isinstance(text, str) is False:
+        raise TypeError('text must be a string')
+
+    for char in text:
+        st += char
+
+        if char in special:
+            print(st.strip())
+            print()
+            st = ''
+
+    print(st.strip(), end='')
