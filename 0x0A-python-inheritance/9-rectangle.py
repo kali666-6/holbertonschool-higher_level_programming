8f029b31123610c52a3292782e7e361ecd7d29f2
root <root@kali-linux>
2021-06-02 22:44:37 +0200

    Python - Inheritance

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""
-more class base
-"""
-
+""" class Rectangle """
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ definition of a Rectangle """
+    """ class Rectangle """
+
     def __init__(self, width, height):
-        """ constructor and width, height"""
+        """ initial attributes """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-        BaseGeometry.integer_validator(self, "width", self.__width)
-        BaseGeometry.integer_validator(self, "height", self.__height)
 
     def area(self):
+        """ Area """
         return self.__width * self.__height
 
     def __str__(self):
-        """ print """
-        return ("[Rectangle] " + str(self.__width) + "/" + str(self.__height))
+        """ str """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
